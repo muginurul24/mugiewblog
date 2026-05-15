4a563227f8f0d859cc4b33f6ffd6973260adbeca
Mugi Nurul Ihksani <iqooz9xmg@gmail.com>
2026-05-15 22:21:35 +0700

    First Commit

--- config/horizon.php
+++ config/horizon.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('HORIZON_NAME'),
+    'name' => env('APP_NAME', 'Laravel') . '-Horizon',
 
     /*
     |--------------------------------------------------------------------------
@@ -69,7 +69,7 @@ return [
 
     'prefix' => env(
         'HORIZON_PREFIX',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_horizon:'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_horizon:'
     ),
 
     /*

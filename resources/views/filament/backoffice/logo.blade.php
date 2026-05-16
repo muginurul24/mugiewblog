293bfddd3d5e1fc5a3384a3cf473a7f092a9adc8
Mugi Nurul Ihksani <iqooz9xmg@gmail.com>
2026-05-16 15:24:50 +0700

    change: logo

--- resources/views/filament/backoffice/logo.blade.php
+++ resources/views/filament/backoffice/logo.blade.php
@@ -1,10 +1,10 @@
 <div class="flex items-center gap-3" data-backoffice-logo>
     <span
-        class="flex h-8 w-8 items-center justify-center rounded-lg bg-primary-500 text-sm font-bold text-white shadow-sm shadow-black/20">
+        class="flex h-8 w-8 items-center justify-center rounded-lg bg-primary-500 text-sm font-bold dark:text-white text-black shadow-sm dark:shadow-black/20 shadow-white/20">
         M
     </span>
 
-    <span class="font-display text-lg font-bold text-white">
-        Mugiew<span class="text-primary-400">Blog</span>
+    <span class="font-display text-lg font-bold dark:text-white text-black">
+        Mugiew<span class="dark:text-primary-400 text-primary-600">Blog</span>
     </span>
 </div>
